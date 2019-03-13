68e9d1876b89af138e170eb51a62b761657517cf
Guillaume Fricker <fricker.g@zoho.com>
2019-03-13 17:23:39 +0100

    Fixed path for project name

--- installers/windows/createinstaller.js
+++ installers/windows/createinstaller.js
@@ -14,7 +14,7 @@ function getInstallerConfig () {
   const outPath = path.join(rootPath, 'release-builds')
 
   return Promise.resolve({
-    appDirectory: path.join(outPath, 'electron-tutorial-app-win32-x64/'),
+    appDirectory: path.join(outPath, 'electron-express-angular-win32-x64/'),
     authors: 'Guillaume Fricker',
     noMsi: true,
     outputDirectory: path.join(outPath, 'windows-installer'),
