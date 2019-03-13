4252c60b0263c069355fa818e58471dacbef0d78
Guillaume Fricker <fricker.g@zoho.com>
2019-03-13 15:11:33 +0100

    Created installation script

---
+++ electron/windows/createinstaller.js
@@ -0,0 +1,25 @@
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
+
+getInstallerConfig()
+  .then(createWindowsInstaller)
+  .catch((error) => {
+    console.error(error.message || error)
+    process.exit(1)
+  })
+
+function getInstallerConfig() {
+  console.log('creating windows installer')
+  const rootPath = path.join('./')
+  const outPath = path.join(rootPath, 'electron')
+
+  return Promise.resolve({
+    appDirectory: path.join(outPath, 'electron-express-angular-darwin-x64/'),
+    authors: 'Guillaume Fricker',
+    noMsi: true,
+    outputDirectory: path.join(outPath, 'thegoat'),
+    exe: 'electron-express-angular.exe',
+    setupExe: 'boilerplate-installer-win32.exe',
+    setupIcon: path.join(rootPath, 'angular', 'src', 'favicon.ico')
+  })
+}
